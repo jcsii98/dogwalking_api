3925a79f3147b3459758731f22c45fb751ad0ea0
Jose Saribong <jose.c.saribong@gmail.com>
2023-10-02 23:24:25 +0800

    permitted kind attribute in signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::API
         include DeviseTokenAuth::Concerns::SetUserByToken
 
         def configure_permitted_parameters
-                devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+                devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :kind])
         end
 end
