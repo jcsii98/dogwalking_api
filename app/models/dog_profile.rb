cc3e6d10bca3541c6c0671d3339eacad9a3e873c
Jose Saribong <jose.c.saribong@gmail.com>
2023-10-03 23:42:05 +0800

    build script

--- app/models/dog_profile.rb
+++ app/models/dog_profile.rb
@@ -1,6 +1,6 @@
 class DogProfile < ApplicationRecord
   belongs_to :user
-  validates :name, presence: true, uniqueness: { scope: :user_id, message: "Name has already been taken for this user." }
+  validates :name, presence: true, uniqueness: { scope: :user_id, message: "has already been taken for this user." }
   validates :age, numericality: { only_integer: true, greater_than: 0 }
   validates :weight, numericality: { only_integer: true, greater_than: 0 }
 
--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,5 +2,9 @@
 # exit on error
 set -o errexit
 
+echo "Starting bundle install..."
 bundle install
+echo "Starting db:setup..."
+bundle exec rake db:setup
+echo "Starting db:migrate..."
 bundle exec rake db:migrate
