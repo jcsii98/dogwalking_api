1fb399a906cb38063c0821d3058199f6d22c1e82
Jose Saribong <jose.c.saribong@gmail.com>
2023-10-03 23:50:16 +0800

    test build

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,7 +4,6 @@ set -o errexit
 
 echo "Starting bundle install..."
 bundle install
-echo "Starting db:setup..."
-bundle exec rake db:setup
+
 echo "Starting db:migrate..."
 bundle exec rake db:migrate
