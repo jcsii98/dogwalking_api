2e160cfb30848aaf86c3ea7a959502913902b657
Jose Saribong <jose.c.saribong@gmail.com>
2023-10-07 21:19:38 +0800

    ws prod config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,7 +35,7 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
-  config.action_cable.url = "wss://dogwalking-app.onrender.com/cable"
+  config.action_cable.url = "wss://dogwalking-api.onrender.com/cable"
   config.action_cable.allowed_request_origins = [ "https://dogwalking-app.onrender.com", "http://localhost:5173/", /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
