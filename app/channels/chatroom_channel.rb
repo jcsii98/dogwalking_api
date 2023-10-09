f1385ac564b998fd99e84af15cc3a5990d5fb9bf
Jose Saribong <jose.c.saribong@gmail.com>
2023-10-09 22:46:44 +0800

    chatroom cache config for prod

--- app/channels/chatroom_channel.rb
+++ app/channels/chatroom_channel.rb
@@ -7,7 +7,7 @@ class ChatroomChannel < ApplicationCable::Channel
     if chatroom.user_associated?(user)
       stream_for chatroom
 
-      online_users(chatroom).add(user.id)
+      add_online_user(chatroom, user.id)
 
       self.class.broadcast_to(chatroom, type: 'users_online', users: online_users(chatroom).to_a)
     else
@@ -18,7 +18,7 @@ class ChatroomChannel < ApplicationCable::Channel
   def unsubscribed
     chatroom = Chatroom.find(params[:id])
 
-    online_users(chatroom).delete(current_user.id)
+    remove_online_user(chatroom, current_user.id)
 
     self.class.broadcast_to(chatroom, type: 'users_online', users: online_users(chatroom).to_a)
   end
@@ -26,6 +26,21 @@ class ChatroomChannel < ApplicationCable::Channel
   private
   
   def online_users(chatroom)
-    Rails.cache.fetch("chatroom_#{chatroom.id}_online_users") { Set.new }
+    cache_key = "chatroom_#{chatroom.id}_online_users"
+    Rails.cache.read(cache_key) || Set.new
+  end
+
+  def add_online_user(chatroom, user_id)
+    cache_key = "chatroom_#{chatroom.id}_online_users"
+    users = online_users(chatroom)
+    users.add(user_id)
+    Rails.cache.write(cache_key, users)
+  end
+
+  def remove_online_user(chatroom, user_id)
+    cache_key = "chatroom_#{chatroom.id}_online_users"
+    users = online_users(chatroom)
+    users.delete(user_id)
+    Rails.cache.write(cache_key, users)
   end
 end
