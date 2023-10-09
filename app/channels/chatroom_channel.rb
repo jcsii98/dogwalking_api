60c353b222452e6bd63f343bf9ca8a2f6d564cb1
Jose Saribong <jose.c.saribong@gmail.com>
2023-10-09 22:19:08 +0800

    fix presence in chatroom response

--- app/channels/chatroom_channel.rb
+++ app/channels/chatroom_channel.rb
@@ -2,13 +2,14 @@ class ChatroomChannel < ApplicationCable::Channel
   def subscribed
     chatroom = Chatroom.find(params[:id])
     
-    # Check if it's a test environment, and set the user accordingly
     user = Rails.env.test? ? User.find(connection.current_user_id) : current_user
-    
+
     if chatroom.user_associated?(user)
       stream_for chatroom
 
-      self.class.broadcast_to(chatroom, type: 'user_connected', user: user.name)
+      online_users(chatroom).add(user.id)
+
+      self.class.broadcast_to(chatroom, type: 'users_online', users: online_users(chatroom).to_a)
     else
       reject
     end
@@ -17,6 +18,14 @@ class ChatroomChannel < ApplicationCable::Channel
   def unsubscribed
     chatroom = Chatroom.find(params[:id])
 
-    self.class.broadcast_to(chatroom, type: 'user_disconnected', user: current_user.name)
+    online_users(chatroom).delete(current_user.id)
+
+    self.class.broadcast_to(chatroom, type: 'users_online', users: online_users(chatroom).to_a)
+  end
+
+  private
+  
+  def online_users(chatroom)
+    Rails.cache.fetch("chatroom_#{chatroom.id}_online_users") { Set.new }
   end
 end
